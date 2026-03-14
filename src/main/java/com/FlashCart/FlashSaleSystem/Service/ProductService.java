39e7f4d7338dc5a829c67eed71a6292f5a23a5de
Om Pawar <om.d.pawar.214@gmail.com>
2026-03-14 22:16:17 +0530

    Added ProductService interface and ProductServiceImpl class will provide the business logic to all the endpoints expose by ProductController ,currently includes logic to fetch all the products prom database and return the converted list of ProductDTOs to the controller

---
+++ src/main/java/com/FlashCart/FlashSaleSystem/Service/ProductService.java
@@ -0,0 +1,9 @@
+package com.FlashCart.FlashSaleSystem.Service;
+
+import com.FlashCart.FlashSaleSystem.DTOs.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+    List<ProductDTO> getAllProducts();
+}
