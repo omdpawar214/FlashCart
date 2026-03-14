bbaf3a2562752506b7b2a110cfdbd8665315f569
Om Pawar <om.d.pawar.214@gmail.com>
2026-03-14 22:16:54 +0530

    Added ProductService interface and ProductServiceImpl class will provide the business logic to all the endpoints expose by ProductController ,currently includes logic to fetch all the products prom database and return the converted list of ProductDTOs to the controller

---
+++ src/main/java/com/FlashCart/FlashSaleSystem/Service/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.FlashCart.FlashSaleSystem.Service;
+
+import com.FlashCart.FlashSaleSystem.DTOs.ProductDTO;
+import com.FlashCart.FlashSaleSystem.Models.Product;
+import com.FlashCart.FlashSaleSystem.Repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    private ProductRepository productRepository;
+   private ModelMapper modelMapper;
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<ProductDTO> getAllProducts() {
+        //fetching all the products from repository
+        List<Product> products = productRepository.findAll();
+        //converting products to productDTO
+        List<ProductDTO> productDTOS = new ArrayList<>();
+        for (Product product:products){
+            productDTOS.add(modelMapper.map(product, ProductDTO.class));
+        }
+        //returning the result
+        return productDTOS;
+    }
+}
