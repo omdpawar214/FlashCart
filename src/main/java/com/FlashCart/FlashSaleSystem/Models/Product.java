f12d959b6a50000f455a16b040964e6bdadb7760
Om Pawar <om.d.pawar.214@gmail.com>
2026-03-14 22:06:56 +0530

    Added product class which will describe the product in our flash sale system

---
+++ src/main/java/com/FlashCart/FlashSaleSystem/Models/Product.java
@@ -0,0 +1,23 @@
+package com.FlashCart.FlashSaleSystem.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+}
