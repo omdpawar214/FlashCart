191fea7a1c816a6bea387a08eb804997128533ab
Om Pawar <om.d.pawar.214@gmail.com>
2026-03-14 22:10:57 +0530

    Added ProductController which expose all the required endpoints for the product module

---
+++ src/main/java/com/FlashCart/FlashSaleSystem/Controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.FlashCart.FlashSaleSystem.Controller;
+
+import com.FlashCart.FlashSaleSystem.DTOs.ProductDTO;
+import com.FlashCart.FlashSaleSystem.Service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    //resolving all the dependency using dependency injection
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //method to get all the products
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> allProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+}
