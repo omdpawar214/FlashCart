eada9e3f1078adfeb3f7206013a1c66f35990d38
Om Pawar <om.d.pawar.214@gmail.com>
2026-04-06 22:48:39 +0530

    Started implementing the redis distributed lock service , which will maintain the lock on the stock reduction and order creation part of purchase api which is critical section of purchase api. this lock will assure only one system can access this api at a time

---
+++ src/main/java/com/FlashCart/FlashSaleSystem/Service/RedisLockService.java
@@ -0,0 +1,24 @@
+package com.FlashCart.FlashSaleSystem.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class RedisLockService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public boolean acquireLock(String key, long timeout) {
+        Boolean success = redisTemplate.opsForValue()
+                .setIfAbsent(key, "locked", Duration.ofMillis(timeout));
+        return Boolean.TRUE.equals(success);
+    }
+
+    public void releaseLock(String key) {
+        redisTemplate.delete(key);
+    }
+}
