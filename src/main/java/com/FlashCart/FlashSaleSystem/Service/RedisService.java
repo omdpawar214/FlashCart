8102e4d1feb3302e435593ad75c198f32388d0a7
Om Pawar <om.d.pawar.214@gmail.com>
2026-04-19 20:53:15 +0530

    Using LUA script in the redis Service to make the decrement stock operation become atomic.

--- src/main/java/com/FlashCart/FlashSaleSystem/Service/RedisService.java
+++ src/main/java/com/FlashCart/FlashSaleSystem/Service/RedisService.java
@@ -2,8 +2,10 @@ package com.FlashCart.FlashSaleSystem.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -11,6 +13,26 @@ public class RedisService {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    //lau script for atomic transactions
+    private static final String STOCK_LUA_SCRIPT =
+            "local stock = tonumber(redis.call('GET', KEYS[1])) " +
+                    "if stock >= tonumber(ARGV[1]) then " +
+                    "   return redis.call('DECRBY', KEYS[1], ARGV[1]) " +
+                    "else " +
+                    "   return -1 " +
+                    "end";
+    public Long decreaseStockAtomically(String key, int quantity) {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        script.setScriptText(STOCK_LUA_SCRIPT);
+        script.setResultType(Long.class);
+
+        return redisTemplate.execute(
+                script,
+                Collections.singletonList(key),
+                String.valueOf(quantity)
+        );
+    }
 
     public void setStock(String key, Integer value) {
         redisTemplate.opsForValue().set(key, String.valueOf(value));
